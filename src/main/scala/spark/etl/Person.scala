634e3d6baf2df4bed16a08d1d906d6d8daef6446
ashish <ashishpatel0720@gmail.com>
2021-06-26 21:52:53 +0530

    [REFACTORING] - done

--- src/main/scala/spark/etl/Person.scala
+++
@@ -1,4 +0,0 @@
-package spark.etl
-
-final case class Person(firstName: String, lastName: String,
-                        country: String, age: Int)
---
+++ src/main/scala/spark/etl/usecases/PeopleDataAnalysis.scala
@@ -0,0 +1,36 @@
+package spark.etl.usecases
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import WordCount.reader
+
+
+
+object PeopleDataAnalysis{
+  def main(args: Array[String]) = {
+    val spark: SparkSession = org.apache.spark.sql.SparkSession.builder()
+      .appName("TEST APP  ")
+      .master("local[*]")
+      .config("option", "some-value")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val version = spark.version
+    println("SPARK VERSION = " + version)
+
+    val sumHundred = spark.range(1, 101).reduce(_ + _)
+    println(f"Sum 1 to 100 = $sumHundred")
+
+    println("Reading from csv file: people-example.csv")
+    val persons = reader.csv("data/csv/people-example.csv").as[Person]
+    persons.show(2)
+    val averageAge = persons.agg(avg("age"))
+                     .first.get(0).asInstanceOf[Double]
+    println(f"Average Age: $averageAge%.2f")
+
+  }
+  final case class Person(firstName: String, lastName: String,
+                          country: String, age: Int)
+
+}
