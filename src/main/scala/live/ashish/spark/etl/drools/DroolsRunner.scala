6133ef5ca0f66fbc499b2c9659174eb0736da4db
Ashish Patel <ashishpatel0720@gmail.com>
2023-08-28 17:25:41 +0530

    fixedformat:v1 working

--- src/main/scala/live/ashish/spark/etl/drools/DroolsRunner.scala
+++ src/main/scala/live/ashish/spark/etl/drools/DroolsRunner.scala
@@ -9,10 +9,6 @@ import org.kie.api.{KieBase, KieServices}
 // use jdk8
 // this creates single session for each partition and per row
 object DroolsRunner {
-//  val ruleFilePath = "data/drools-input/person_rules.drl"
-//  val ruleFilePath = "src/main/resources/rules/person_rules2.drl"
-  val ruleFilePath = "rules/person_rules2.drl"
-
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("DroolsRuleEngine")
@@ -20,13 +16,11 @@ object DroolsRunner {
       .getOrCreate()
 
     val kieServices = KieServices.Factory.get()
-    //    val drlContent = scala.io.Source.fromFile(ruleFilePath).mkString
-    //    println(drlContent)
-    //    val kieFileSystem = kieServices.newKieFileSystem()
-//      .write(ruleFilePath, kieServices.getResources.newByteArrayResource(drlContent.getBytes))
-    val kieFileSystem = kieServices.newKieFileSystem()
-      .write(ruleFilePath, kieServices.getResources.newClassPathResource(ruleFilePath));
-//      .write(ruleFilePath, kieServices.getResources.newClassPathResource("live.ashish.spark.etl.droolsperson_rules2.drl"));
+    val ruleFilePathFull = "src/main/resources/rules/person_rules2.drl"
+    val drlContent = scala.io.Source.fromFile(ruleFilePathFull).mkString
+        println(drlContent)
+        val kieFileSystem = kieServices.newKieFileSystem()
+      .write(ruleFilePathFull, kieServices.getResources.newByteArrayResource(drlContent.getBytes))
 
     val kieBuilder = kieServices.newKieBuilder(kieFileSystem)
     val results = kieBuilder.buildAll()
